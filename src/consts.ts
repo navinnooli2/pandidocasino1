0c55e20fda95d8e228dc263a6f59bdb1bc342fae
navinnooli2 <navinnooli2@gmail.com>
2025-11-19 11:33:27 +0700

    casino site

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Pandido Casino";
+export const SITE_DESCRIPTION = "Profitez d'un bonus de bienvenue de 200% jusqu'à €5,000 et découvrez plus de 1800 jeux de casino en ligne.";
+export const AFFILIATE_LINK = "https://m-traff.net/yk5QY2vs";
