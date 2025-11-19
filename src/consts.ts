df0472a4b19a1859b01079e533634d80bb5e6fce
navinnooli2 <navinnooli2@gmail.com>
2025-11-19 14:27:01 +0700

    NDD

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Pandido Casino";
-export const SITE_DESCRIPTION = "Profitez d'un bonus de bienvenue de 200% jusqu'à €5,000 et découvrez plus de 1800 jeux de casino en ligne.";
+export const SITE_TITLE = "Pandido Casino - Bonus 200% jusqu'à €5,000 | Casino en Ligne Français";
+export const SITE_DESCRIPTION = "Pandido Casino : Casino en ligne français avec bonus de bienvenue 200% jusqu'à €5,000. Plus de 1800 jeux de casino, retraits rapides et service client 24/7. Rejoignez Pandido Casino aujourd'hui !";
 export const AFFILIATE_LINK = "https://m-traff.net/yk5QY2vs";
